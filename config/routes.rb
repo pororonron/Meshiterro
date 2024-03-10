a17ccf5e015375a93cd09a3f29e1f7ff8345b956
pororonron <153398697+pororonron@users.noreply.github.com>
2024-03-10 23:03:23 +0000

    [Add] 管理者用画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'homes#top'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create, :destroy]
-    resource :favorite, only: [:create, :destroy]
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
-  resources :users, only: [:show, :edit, :update]
-  get 'homes/about' => 'homes#about', as: 'about'
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+    get 'homes/about' => 'homes#about', as: 'about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
